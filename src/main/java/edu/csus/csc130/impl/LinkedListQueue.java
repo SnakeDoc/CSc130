3ca83bfab4924b432dd27562c0b4ad72670605e2
Jason <alupis1@gmail.com>
2015-02-15 16:10:48 -0800

    Add LinkedListQueue implementation

--- src/main/java/edu/csus/csc130/impl/LinkedListQueue.java
+++ src/main/java/edu/csus/csc130/impl/LinkedListQueue.java
@@ -5,37 +5,64 @@ import edu.csus.csc130.Queue;
 import java.util.Iterator;
 
 /**
- * Created by FirstName LastName
+ * Created by Jason Sipula
  * Circular linked list implementation of Queue
  */
 public class LinkedListQueue<E> implements Queue<E> {
+	
+	private int N;
+	private Node first;
+	private Node last;
+	
+	private class Node {
+		private E item;
+		private Node next;
+	}
+	
     public LinkedListQueue() {
-
+    	first = new Node();
+    	last = null;
+    	N = 0;
     }
 
     @Override
     public void enqueue(E item) {
-
+    	Node oldLast = last;
+    	last = new Node();
+    	last.item = item;
+    	last.next = null;
+    	if (isEmpty())
+    		first = last;
+    	else
+    		oldLast.next = last;
+    	N++;
     }
 
     @Override
     public E dequeue() {
-        return null;
+		E item = (E) first.item;
+		if (first.next == null)
+			first = new Node();
+		else
+			first = first.next;
+        N--;
+        if (isEmpty()) last = null;
+        return item;
     }
 
     @Override
     public E peek() {
-        return null;
+        return first.item;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return first.item == null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return N;
     }
 
     /**
@@ -44,6 +71,28 @@ public class LinkedListQueue<E> implements Queue<E> {
      */
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new ListIterator();
+    }
+    
+    private class ListIterator implements Iterator<E> {
+    	private Node current = first;
+    	
+    	@Override
+    	public boolean hasNext() {
+    		return current != null;
+    	}
+    	
+    	@Override
+    	public void remove() {
+    		throw new UnsupportedOperationException();
+    	}
+    	
+    	@SuppressWarnings("unchecked")
+		@Override
+    	public E next() {
+    		Object item = current.item;
+    		current = current.next;
+    		return (E) item;
+    	}
     }
 }
